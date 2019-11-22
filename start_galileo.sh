6c25c9296ab4ca173c7a9302c92bc0ad908a64d8
Oidlichtnwoada <hannes.brantner@hotmail.com>
2019-11-22 21:48:00 +0000

    update routine for raspberry pi added

--- start_galileo.sh
+++ start_galileo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -rf /home/pi/Galileo/__pycache__
+/home/pi/Galileo/update_pi.sh
 git -C /home/pi/Galileo fetch
 git -C /home/pi/Galileo pull
 python3 /home/pi/Galileo/start_galileo.py
---
+++ update_pi.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+apt-get update
+apt-get upgrade
+apt-get full-upgrade
+apt-get dist-upgrade
+apt-get autoremove
+apt-get autoclean
+apt update
+apt upgrade
+apt full-upgrade
+apt dist-upgrade
+apt autoremove
+apt autoclean
